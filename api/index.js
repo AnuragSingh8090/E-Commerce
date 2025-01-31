4470c6c5c4c56b29c0e426e97288561234a9b633
Anurag80909086 <anurag.singh26@amity.edu>
2025-01-31 11:09:33 +0530

    backend test

--- api/index.js
+++ api/index.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(5000, () => {
   console.log("Server running on port: 5000");
 });
