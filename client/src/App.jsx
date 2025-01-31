09dbdf885456b55cc3c85ddef9edca79fbc762d0
Anurag80909086 <anurag.singh26@amity.edu>
2025-01-31 11:06:08 +0530

    Testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,10 @@
 function App() {
-  return <div className="bg-red-500">App</div>;
+  return (
+    <div className="bg-red-500">
+      App
+      <span>Testing Connection</span>
+    </div>
+  );
 }
 
 export default App;
