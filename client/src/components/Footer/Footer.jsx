a33275a6cc3eeac930086a93f5636f86cd5b8c0b
Anuragkrenai <anurag@krenai.com>
2025-03-21 09:38:01 +0530

    navbar half portion done

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,82 +2,115 @@ import "./Footer.css";
 import { Link } from "react-router-dom";
 const Footer = () => {
   return (
-    <div className="footer bg-purple-900  w-full text-[white] p-[30px] flex flex-col items-center gap-[40px]">
-      <section className="flex gap-10px items-center  justify-between w-[60%]">
-        <div className=" w-max">
-          <h2 className="font-bold ">Product</h2>
-          <ul className=" text-gray-300 text-[14px] mt-2">
-            <Link to="/">
-              <li className="hover:text-white">Features</li>
-            </Link>
-            <Link to="/">
-              <li className="hover:text-white">Services</li>
-            </Link>
-            <Link to="/">
-              <li className="hover:text-white">Enterprise</li>
-            </Link>
-            <Link to="/">
-              <li className="hover:text-white">Pricing</li>
-            </Link>
-          </ul>
-        </div>
-        <div className=" w-max">
-          <h2 className="font-bold ">Company</h2>
-          <ul className=" text-gray-300 text-[14px] mt-2">
-            <Link to="/">
-              <li className="hover:text-white">Features</li>
-            </Link>
-            <Link to="/">
-              <li className="hover:text-white">Services</li>
-            </Link>
-            <Link to="/">
-              <li className="hover:text-white">Enterprise</li>
-            </Link>
-            <Link to="/">
-              <li className="hover:text-white">Pricing</li>
-            </Link>
-          </ul>
-        </div>
-        <div className=" w-max">
-          <h2 className="font-bold ">Help Center</h2>
-          <ul className=" text-gray-300 text-[14px] mt-2">
-            <Link to="/">
-              <li className="hover:text-white">Features</li>
-            </Link>
-            <Link to="/">
-              <li className="hover:text-white">Services</li>
-            </Link>
-            <Link to="/">
-              <li className="hover:text-white">Enterprise</li>
-            </Link>
-            <Link to="/">
-              <li className="hover:text-white">Pricing</li>
-            </Link>
-          </ul>
-        </div>
-        <div className=" w-max">
-          <h2 className="font-bold ">Partners</h2>
-          <ul className=" text-gray-300 text-[14px] mt-2">
-            <Link to="/">
-              <li className="hover:text-white">Features</li>
-            </Link>
-            <Link to="/">
-              <li className="hover:text-white">Services</li>
-            </Link>
-            <Link to="/">
-              <li className="hover:text-white">Enterprise</li>
-            </Link>
-            <Link to="/">
-              <li className="hover:text-white">Pricing</li>
-            </Link>
-          </ul>
-        </div>
-      </section>
+    <div className="footer bg-[var(--primary)]  w-full text-[white] px-[15px] py-[10px] flex flex-col items-center md:px-[30px] md:py-[20px] lg:py-[10px]">
+      <div className="w-full lg:w-[90%] xl:w-[80%]">
+        <section className="flex gap-2 flex-wrap align-center justify-between  mb-3 pb-3 w-full border-b-1 border-[#54bef7] md:flex-nowrap md:justify-center md:gap-18 lg:justify-between lg:gap-2 ">
+          <div className=" w-max">
+            <h2 className="font-medium w-max text-[16px] py-1 border-b-1 border-[white]">
+              Quick Links
+            </h2>
+            <ul className=" text-gray-300 flex flex-col gap-1 text-[14px] font-light mt-2 ">
+              <Link to="/">
+                <li className="hover:text-white">Electronics</li>
+              </Link>
+              <Link to="/">
+                <li className="hover:text-white">Clothing</li>
+              </Link>
+              <Link to="/">
+                <li className="hover:text-white">Home Appliences</li>
+              </Link>
+              <Link to="/">
+                <li className="hover:text-white">Kids</li>
+              </Link>
+              <Link to="/">
+                <li className="hover:text-white">Beauty</li>
+              </Link>
+              <Link to="/">
+                <li className="hover:text-white">Kitchen</li>
+              </Link>
+              <Link to="/">
+                <li className="hover:text-white">Personal Care</li>
+              </Link>
+            </ul>
+          </div>
 
-      <p className="text-gray-400 text-center">
-        Copyright ©2025 All rights reserved | Block is made with by{" "}
-        <b>Anurag Kumar Singh</b>
-      </p>
+          <div className=" w-max">
+            <h2 className="font-medium w-max text-[16px] py-1 border-b-1 border-[white]">
+              Customer Services
+            </h2>
+            <ul className=" text-gray-300 flex flex-col gap-1 text-[14px] font-light mt-2 ">
+              <Link to="/">
+                <li className="hover:text-white">About US</li>
+              </Link>
+              <Link to="/">
+                <li className="hover:text-white">Terms & Conditions</li>
+              </Link>
+              <Link to="/">
+                <li className="hover:text-white">FAQ</li>
+              </Link>
+              <Link to="/">
+                <li className="hover:text-white">Privace Policy</li>
+              </Link>
+              <Link to="/">
+                <li className="hover:text-white">
+                  Cancellation & Return Policy
+                </li>
+              </Link>
+            </ul>
+          </div>
+
+          <div className=" w-max">
+            <h2 className="font-medium text-[16px] w-max py-1 border-b-1 border-[white]">
+              Contact US
+            </h2>
+            <ul className=" text-gray-300  flex  flex-col gap-1   text-[14px] font-light mt-2 ">
+              <li className="hover:text-white flex items-center gap-2">
+                <i class="fa-solid fa-phone text-12"></i>+91 8090674352
+              </li>
+
+              <li className="hover:text-white flex items-center gap-2">
+                <i class="fa-brands fa-whatsapp text-12"></i>+91 8090674352
+              </li>
+
+              <li className="hover:text-white flex items-center gap-2">
+                <i class="fa-solid fa-envelope text-12 "></i>
+                anuragkumarsingh154@gmail.com
+              </li>
+            </ul>
+            <div className="flex items-center gap-3 mt-4">
+              <a href="#">
+                <i
+                  class="text-gray-300 text-[20px] pointer hover:text-white fa-brands fa-instagram"
+                  title="Instagram"
+                ></i>
+              </a>
+              <a href="#">
+                <i
+                  class="text-gray-300 text-[20px] pointer hover:text-white fa-brands fa-facebook"
+                  title="Facebook"
+                ></i>
+              </a>
+              <a href="#">
+                <i
+                  class="text-gray-300 text-[20px] pointer hover:text-white fa-brands fa-linkedin"
+                  title="Linkedin"
+                ></i>
+              </a>
+              <a href="#">
+                <i
+                  class="text-gray-300 text-[20px] pointer hover:text-white fa-brands fa-x-twitter"
+                  title="Twitter"
+                ></i>
+              </a>
+            </div>
+          </div>
+        </section>
+
+        <p className="text-[#f2f3f3] font-light text-[12px] w-full  text-center">
+          Copyright ©2025 All rights reserved. BaazaarWale pvt Ltd. | Service is
+          made by <b>Anurag Kumar Singh</b>
+        </p>
+      </div>
     </div>
   );
 };
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -53,202 +53,271 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className="navbar sticky z-99 top-0 left-0 flex items-center justify-between gap-[10px] px-[30px] py-0 bg-[white] shadow-md">
-      <div
-        className="bars_container items-center justify-center hidden"
-        onClick={openNavbar}
-      >
-        <i className="fa-solid fa-bars text-[30px] text-[#333232] active:scale-[0.95]  cursor-pointer"></i>
+    <nav className="navbar sticky z-99 top-0 left-0  flex flex-col items-center justify-between gap-[10px] px-[15px] py-[0px] bg-[white] shadow-md">
+      <div className="w-full flex items-center mt-[5px] justify-between gap-[20px] relative">
+        <div className="logoContainer centerFlex gap-[10px] absolute top-0 left-0 md:static">
+          <div
+            className="bars_container items-center justify-center text-[25px] text-[var(--primary)] active:scale-[0.95]  cursor-pointer md:hidden"
+            onClick={openNavbar}
+          >
+            <i className="fa-solid fa-bars "></i>
+          </div>
+          <NavLink to="/" className="centerFlex gap-[10px]">
+            <div className="navLogo w-[43px] flex-shrink-0 ">
+              <img src="/brand-logo.png" alt="Logo" />
+            </div>
+            <span
+              className="text-[22px] "
+              style={{
+                fontFamily: "var(--custom-font)",
+                background: "linear-gradient(to right, #008ecc, #d26c1e)",
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+              }}
+            >
+              BazaarWale
+            </span>
+          </NavLink>
+        </div>
+
+        <div className="w-full flex flex-wrap-reverse items-center justify-end gap-[10px] my-[7px] md:flex-nowrap">
+          <div className="w-full flex items-center  gap-[20px]">
+            <div className="inputContainer flex items-center justify-center search_container w-full text-[13px]  rounded-lg px-3 py-2 bg-[var(--primary-light)]  gap-[7px]">
+              <i
+                className="fa-solid fa-magnifying-glass active:scale-[0.95] text-[var(--primary)] cursor-pointer"
+                onClick={handleSearch}
+              ></i>
+              <input
+                type="text"
+                placeholder="Search eg. iphone 15, laptop, etc."
+                className="w-full"
+                onChange={(e) => setSearchText(e.target.value)}
+                value={searchText}
+                onKeyDown={(e) => (e.key === "Enter" ? handleSearch() : null)}
+              />
+            </div>
+
+            <div className="cart_container relative active:scale-[0.95] text-[20px] cursor-pointer text-[#333232]">
+              <NavLink to="/cart">
+                <div className="centerFlex gap-2">
+                  <div className="relative">
+                    <i
+                      className="fa-solid text-[var(--primary)] text-[17px] fa-cart-shopping"
+                      title="Cart"
+                    ></i>
+                    <span className="absolute top-0 right-[-30%] select-none centerFlex bg-[#d63909] text-white text-[11px] centerFlex  rounded-[50%] h-[14px] w-[14px]">
+                      {cartItems}
+                    </span>
+                  </div>
+                  <span className="text-[15px] ">Cart</span>
+                </div>
+              </NavLink>
+            </div>
+          </div>
+
+          <div className="userSection shrink-0 flex justify-center items-center gap-[30px] ">
+            {/* <div className="theme_container active:scale-[0.95] text-[20px] cursor-pointer text-[#333232]">
+            <i className="fa-solid fa-moon" title="Dark Mode"></i>
+          </div> */}
+
+            <div className="user_container relative  text-[17px]  text-[#333232]">
+              <div
+                className="centerFlex gap-2 cursor-pointer select-none"
+                onClick={() =>
+                  showDrop ? hideAccountMenu() : showAccountMenu()
+                }
+              >
+                <i
+                  className="fa-regular text-[var(--primary)]  fa-user active:scale-[0.95] cursor-pointer"
+                  title="Account"
+                ></i>
+                <span className="text-[16px] ">Log in</span>
+              </div>
+              {showDrop && isLoggedIn ? (
+                <div className="dropContainer absolute top-[40px] right-[-5px]   bg-white px-[15px] py-[8px] rounded-[6px] boxShadow-light">
+                  <div className="flex w-max gap-[5px] flex-col items-start shrink-0 text-[15px] text-[#333232]">
+                    <NavLink to="/account">
+                      <span
+                        className="active:scale-[0.95] select-none cursor-pointer"
+                        onClick={hideAccountMenu}
+                      >
+                        <i className="fa-regular fa-user mr-[6px]"></i> My
+                        Account
+                      </span>
+                    </NavLink>
+
+                    <NavLink to="/orders">
+                      <span
+                        className="active:scale-[0.95] select-none cursor-pointer"
+                        onClick={hideAccountMenu}
+                      >
+                        <i className="fa-solid fa-box mr-[6px]"></i> My Orders
+                      </span>
+                    </NavLink>
+
+                    <NavLink to="/login">
+                      <span
+                        className="active:scale-[0.95] select-none cursor-pointer text-[#006100]"
+                        onClick={hideAccountMenu}
+                      >
+                        <i className="fa-solid fa-right-to-bracket mr-[5px]"></i>{" "}
+                        Login
+                      </span>
+                    </NavLink>
+
+                    <span
+                      className="active:scale-[0.95] select-none cursor-pointer text-[#ce0303]"
+                      onClick={() => setLoginPopup(true)}
+                    >
+                      <i className="fa-solid fa-right-from-bracket mr-[5px]"></i>{" "}
+                      Log Out
+                    </span>
+
+                    {loginPopup ? (
+                      <div className="logoutPopup z-[9999] fixed top-[0] left-[0] h-screen w-screen flex items-center justify-center  bg-[#00000093]">
+                        <div className=" bg-white p-[10px] px-[20px] flex flex-col items-center justify-center gap-[20px] rounded-[10px] boxShadow-light">
+                          <h2 className="text-center text-[18px]">
+                            Do you want to log out?
+                          </h2>
+                          <div className="flex items-center justify-center gap-[20px] w-full ">
+                            <button
+                              onClick={handleLogout}
+                              className="bg-red-600 py-[5px] px-[25px] cursor-pointer active:scale-[0.95] border-none rounded-[7px] text-white"
+                            >
+                              Yes
+                            </button>
+                            <button
+                              onClick={() => setLoginPopup(false)}
+                              className="bg-green-600 py-[5px] px-[25px] cursor-pointer active:scale-[0.95] border-none rounded-[7px] text-white"
+                            >
+                              No
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+        </div>
       </div>
 
-      <div className="navLogo w-[60px] flex-shrink-0 ">
-        <NavLink to="/">
-          <img src="/Logo.png" alt="Logo" />
-        </NavLink>
-      </div>
-
-      <ul
-        className="flex text-[15px] shrink-0 font-[500] text-[#333232] items-center justify-between select-none gap-[22px]"
-        ref={ulReffrence}
-      >
+      <ul className="hidden  navItems w-full  py-[8px]  items-center  text-[14px] font-[500] text-[#4f4f4f]  select-none gap-[10px] border-t-[1px] border-[var(--border-light)] md:flex md:flex-wrap md:static">
         <div
           className="navBar absolute top-[15px] right-[15px] hidden "
           onClick={closeNavbar}
         >
           <i className="fa-solid fa-xmark text-[black] text-3xl  active:scale-[0.95]"></i>
         </div>
-        <li className="py-[4px] hover:text-[var(--primary)] hover:border-t-2 hover:border-[var(--primary)]  ">
-          <NavLink to="/" className="flex items-center justify-center">
-            <i className="fa-solid fa-house ulIcons mr-[10px]"></i>
+
+        <NavLink
+          to="/"
+          className="py-[3px] px-[10px] shrink-0 hover:text-[var(--primary)] hover:rounded-lg hover:bg-[var(--primary-light)]"
+        >
+          <li className="centerFlex gap-[7px]">
+            <i className="fa-solid fa-house ulIcons "></i>
             Home
-          </NavLink>
-        </li>
-        <li className="py-[4px] hover:text-[var(--primary)] hover:border-t-2 hover:border-[var(--primary)]">
-          <NavLink
-            to="/electronics"
-            className="flex items-center justify-center"
-          >
-            <i className="fa-solid fa-mobile ulIcons mr-[10px]"></i>
+          </li>
+        </NavLink>
+
+        <NavLink
+          to="/electronics"
+          className="py-[3px] px-[10px] shrink-0 hover:text-[var(--primary)] hover:rounded-lg hover:bg-[var(--primary-light)]"
+        >
+          <li className="centerFlex gap-[7px]">
+            <i class="fa-solid fa-mobile-screen-button ulIcons"></i>
             Electronics
-          </NavLink>
-        </li>
-        <li className="py-[4px] hover:text-[var(--primary)] hover:border-t-2 hover:border-[var(--primary)] ">
-          <NavLink to="/beauty" className="flex items-center justify-center">
-            <i className="fa-solid fa-paintbrush ulIcons mr-[10px]"></i>
-            Beauty
-          </NavLink>
-        </li>
-        <li className="py-[4px] hover:text-[var(--primary)] hover:border-t-2 hover:border-[var(--primary)] ">
-          <NavLink to="/clothing" className="flex items-center justify-center">
-            <i className="fa-solid fa-shirt ulIcons mr-[10px]"></i>
+          </li>
+        </NavLink>
+
+        <NavLink
+          to="/clothing"
+          className="py-[3px] px-[10px] shrink-0 hover:text-[var(--primary)] hover:rounded-lg hover:bg-[var(--primary-light)]"
+        >
+          <li className="centerFlex gap-[7px]">
+            <i class="fa-solid fa-shirt ulIcons"></i>
             Clothing
-          </NavLink>
-        </li>
-        <li className="py-[4px] hover:text-[var(--primary)] hover:border-t-2 hover:border-[var(--primary)] ">
-          <NavLink to="/kids" className="flex items-center justify-center">
-            <i className="fa-solid fa-children ulIcons mr-[10px]"></i>
+          </li>
+        </NavLink>
+
+        <NavLink
+          to="/kids"
+          className="py-[3px] px-[10px] shrink-0 hover:text-[var(--primary)] hover:rounded-lg hover:bg-[var(--primary-light)]"
+        >
+          <li className="centerFlex gap-[7px]">
+            <i class="fa-solid fa-children ulIcons"></i>
             Kids
-          </NavLink>
-        </li>
-        <li className="py-[4px] hover:text-[var(--primary)] hover:border-t-2 hover:border-[var(--primary)] ">
-          <NavLink
-            to="/home_appliances"
-            className="flex items-center justify-center"
-          >
-            <i className="fa-solid fa-kitchen-set ulIcons mr-[10px]"></i>
-            Home & Appliances
-          </NavLink>
-        </li>
-        <li className="py-[4px] hover:text-[var(--primary)] hover:border-t-2 hover:border-[var(--primary)] ">
-          <NavLink to="/contact" className="flex items-center justify-center">
-            <i className="fa-solid fa-envelope ulIcons mr-[10px]"></i>
-            Contact Us
-          </NavLink>
-        </li>
+          </li>
+        </NavLink>
+
+        <NavLink
+          to="/beauty"
+          className="py-[3px] px-[10px] shrink-0 hover:text-[var(--primary)] hover:rounded-lg hover:bg-[var(--primary-light)]"
+        >
+          <li className="centerFlex gap-[7px]">
+            <i class="fa-solid fa-spa ulIcons"></i>
+            Beauty
+          </li>
+        </NavLink>
+
+        <NavLink
+          to="/home_appliences"
+          className="py-[3px] px-[10px] shrink-0 hover:text-[var(--primary)] hover:rounded-lg hover:bg-[var(--primary-light)]"
+        >
+          <li className="centerFlex gap-[7px]">
+            <i className="fa-solid fa-plug ulIcons "></i>
+            Home Appliences
+          </li>
+        </NavLink>
+
+        <NavLink
+          to="/kitchen"
+          className="py-[3px] px-[10px] shrink-0 hover:text-[var(--primary)] hover:rounded-lg hover:bg-[var(--primary-light)]"
+        >
+          <li className="centerFlex gap-[7px]">
+            <i className="fa-solid fa-kitchen-set ulIcons "></i>
+            Kitchen
+          </li>
+        </NavLink>
+
+        <NavLink
+          to="/personal_care"
+          className="py-[3px] px-[10px] shrink-0 hover:text-[var(--primary)] hover:rounded-lg hover:bg-[var(--primary-light)]"
+        >
+          <li className="centerFlex gap-[7px]">
+            <i class="fa-solid fa-soap ulIcons"></i>
+            Personal Care
+          </li>
+        </NavLink>
+
+        <NavLink
+          to="/support"
+          className="py-[3px] px-[10px] shrink-0 hover:text-[var(--primary)] hover:rounded-lg hover:bg-[var(--primary-light)]"
+        >
+          <li className="centerFlex gap-[7px]">
+            <i class="fa-solid fa-headset ulIcons"></i>
+            Support
+          </li>
+        </NavLink>
+
+        <NavLink
+          to="/about_us"
+          className="py-[3px] px-[10px] shrink-0 hover:text-[var(--primary)] hover:rounded-lg hover:bg-[var(--primary-light)]"
+        >
+          <li className="centerFlex gap-[7px]">
+            <i className="fa-solid fa-building ulIcons "></i>
+            About Us
+          </li>
+        </NavLink>
       </ul>
 
-      <div
-        className="search_container text-[13px] flex items-center justify-center rounded-lg px-3 py-2 bg-[var(--primary-light)]  gap-[7px]  w-[30%]"
-        title="Search"
-      >
-        <i
-          className="fa-solid fa-magnifying-glass active:scale-[0.95] text-[var(--primary)] cursor-pointer"
-          onClick={handleSearch}
-        ></i>
-        <input
-          type="text"
-          placeholder="Search Item"
-          className="w-full"
-          onChange={(e) => setSearchText(e.target.value)}
-          value={searchText}
-          onKeyDown={(e) => (e.key === "Enter" ? handleSearch() : null)}
-        />
-      </div>
-
-      <div className="userSection flex justify-center items-center gap-[30px] ">
-        {/* <div className="theme_container active:scale-[0.95] text-[20px] cursor-pointer text-[#333232]">
-          <i className="fa-solid fa-moon" title="Dark Mode"></i>
-        </div> */}
-
-        <div className="cart_container relative active:scale-[0.95] text-[20px] cursor-pointer text-[#333232]">
-          <NavLink to="/cart">
-            <div className="centerFlex gap-2">
-              <div className="relative">
-                <i
-                  className="fa-solid text-[var(--primary)] text-[17px] fa-cart-shopping"
-                  title="Cart"
-                ></i>
-                <span className="absolute top-0 left-[-50%] select-none centerFlex bg-[#d63909] text-white text-[11px] centerFlex  rounded-[50%] h-[18px] w-[18px]">
-                  {cartItems}
-                </span>
-              </div>
-              <span className="text-[15px] ">Cart</span>
-            </div>
-          </NavLink>
-        </div>
-
-        <div className="user_container relative  text-[17px]  text-[#333232]">
-          <div
-            className="centerFlex gap-2 cursor-pointer select-none"
-            onClick={() => (showDrop ? hideAccountMenu() : showAccountMenu())}
-          >
-            <i
-              className="fa-regular text-[var(--primary)]  fa-user active:scale-[0.95] cursor-pointer"
-              title="Account"
-            ></i>
-            <span className="text-[15px] ">Log in</span>
-          </div>
-          {showDrop && isLoggedIn ? (
-            <div className="dropContainer absolute top-[40px] right-[-5px]   bg-white px-[15px] py-[8px] rounded-[6px] boxShadow-light">
-              <div className="flex w-max gap-[5px] flex-col items-start shrink-0 text-[15px] text-[#333232]">
-                <NavLink to="/account">
-                  <span
-                    className="active:scale-[0.95] select-none cursor-pointer"
-                    onClick={hideAccountMenu}
-                  >
-                    <i className="fa-regular fa-user mr-[6px]"></i> My Account
-                  </span>
-                </NavLink>
-
-                <NavLink to="/orders">
-                  <span
-                    className="active:scale-[0.95] select-none cursor-pointer"
-                    onClick={hideAccountMenu}
-                  >
-                    <i className="fa-solid fa-box mr-[6px]"></i> My Orders
-                  </span>
-                </NavLink>
-
-                <NavLink to="/login">
-                  <span
-                    className="active:scale-[0.95] select-none cursor-pointer text-[#006100]"
-                    onClick={hideAccountMenu}
-                  >
-                    <i className="fa-solid fa-right-to-bracket mr-[5px]"></i>{" "}
-                    Login
-                  </span>
-                </NavLink>
-
-                <span
-                  className="active:scale-[0.95] select-none cursor-pointer text-[#ce0303]"
-                  onClick={() => setLoginPopup(true)}
-                >
-                  <i className="fa-solid fa-right-from-bracket mr-[5px]"></i>{" "}
-                  Log Out
-                </span>
-
-                {loginPopup ? (
-                  <div className="logoutPopup z-[9999] fixed top-[0] left-[0] h-screen w-screen flex items-center justify-center  bg-[#00000093]">
-                    <div className=" bg-white p-[10px] px-[20px] flex flex-col items-center justify-center gap-[20px] rounded-[10px] boxShadow-light">
-                      <h2 className="text-center text-[18px]">
-                        Do you want to log out?
-                      </h2>
-                      <div className="flex items-center justify-center gap-[20px] w-full ">
-                        <button
-                          onClick={handleLogout}
-                          className="bg-red-600 py-[5px] px-[25px] cursor-pointer active:scale-[0.95] border-none rounded-[7px] text-white"
-                        >
-                          Yes
-                        </button>
-                        <button
-                          onClick={() => setLoginPopup(false)}
-                          className="bg-green-600 py-[5px] px-[25px] cursor-pointer active:scale-[0.95] border-none rounded-[7px] text-white"
-                        >
-                          No
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                ) : (
-                  ""
-                )}
-              </div>
-            </div>
-          ) : (
-            ""
-          )}
-        </div>
-      </div>
+      <div className="hidden"></div>
     </nav>
   );
 };
